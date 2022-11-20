b18f0fd69700ed5fd9dda12da11f878f5a96cf91
denerbatista <denerbatista@icloud.com>
2022-11-20 00:20:25 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+export default {
+  roots: ['<rootDir>/src'],
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // The test environment that will be used for testing
+  testEnvironment: 'jest-environment-node',
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
